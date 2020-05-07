6752985f2a6ddbce3edf6c8baadcaf3b9b101472
Lynda Qiu-Whelan <Lynda.Qiu@numerator.com>
2020-05-07 15:28:36 -0700

    Add print after finishing loading data
    Indication of the finishing step after data is loaded completely.

--- bikeshare.py
+++ bikeshare.py
@@ -78,6 +78,7 @@ def load_data(city, month, day):
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
 
+    print('Finish loading data...')
     return df
 
 
